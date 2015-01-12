af3b32de5a30916a5f88af1732a12c88da0dbf4c
William Wong <souleni5k@Williams-MacBook-Pro.local>
2015-01-11 21:59:01 -0500

    added home page and website foundation structure

---
+++ js/app.js
@@ -0,0 +1,29 @@
+(function() {
+	var app = angular.module( "main", [ ]);
+
+	app.controller("MainController", function($scope, $window) {
+		
+		//captures the height from $window using jquery
+		var height = $(window).height();
+		var buttonHeight = $('#createBtn').height();
+
+		//quick hacky way to find dynamic position
+		$('.modal').css("display","block");
+		var createModalHeight = $('#modalDialog').height();
+		$('.modal').css("display","none");
+
+		//vertically aligns the Create and Receive buttons in the center
+		$('#buttonGroup').css("padding-top", (height-buttonHeight)/2);
+		//same for create modal
+		$(".modal-dialog").css("margin-top", (height-createModalHeight)/2);
+		$(".modal-dialog").css("margin-left", "auto");
+
+		//resize function: on resize, always keep Create and Receive buttons vertically aligned in the center
+		$(window).resize(function() {
+			$('#buttonGroup').css("padding-top", $(window).height() / 2);
+		});
+	});
+
+	//var helloGreeting = "hi";
+
+})();
