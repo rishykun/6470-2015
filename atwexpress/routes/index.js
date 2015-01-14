6e4038f26636ec4081f5897db98a141c7379e8bf
David Wu <davidwu@mit.edu>
2015-01-14 18:47:40 -0500

    moved miniupload thing added a route to the page -> /upload takes you there

--- atwexpress/routes/index.js
+++ atwexpress/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
 
 module.exports = router;
+
+/* Get upload page. */
+router.get('/upload',function(req,res){
+	res.render('upload',{title: 'Upload'})
+});
