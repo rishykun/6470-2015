a9d248577f4bb9d46198eb01e887579a59166fd3
William G Wong <wgwong@mit.edu>
2015-01-19 02:07:54 -0500

    added ui-bootstrap module

--- atwexpress/public/js/profile.js
+++ atwexpress/public/js/profile.js
@@ -4,45 +4,8 @@
 	]);
 
 	app.controller ( 'profileController', function profileController ($scope, $http, $window) {
-		
-		$scope.getUserBoxes = function (user) {
-			user = $scope.userObject.local.email; //debug
-			reqData = {
-				'username':  user,
-			}
-			$http.post('/getuserconfig', reqData)
-				.success (function(data) {
-					$scope.userinfo = JSON.parse(data);
-					$scope.boxes_created =$scope.userinfo.boxes_created;
-					$scope.boxes_collaborated = $scope.userinfo.boxes_collaborated;
-					$scope.$parent.numboxes = $scope.userinfo.boxes_created.length + $scope.userinfo.boxes_collaborated.length;
-					$scope.$parent.Created = [];
-					$scope.$parent.Collaborated = [];
-//------------------------   retrieve box config ------------------
-				for(i=0;i< $scope.boxes_created.length;i++)
-					{
-						console.log($scope.boxes_created[i]);
-						$scope.getBoxConfig($scope.boxes_created[i],true);
-					}
-				//Don't move on unless all post requests are completed or some fail
-				for(i=0;i< $scope.boxes_collaborated.length;i++)
-					{
-						console.log($scope.boxes_collaborated[i],false);
-						$scope.getBoxConfig($scope.boxes_collaborated[i]);
-					}
-				
-				
-
-
-//------------------------------------------------------------------
-				})
-				.error (function() {
-					console.log("Error getting user config file for " + user + "!");
-				});
-		}
-		$scope.getUserBoxes();
-
-
-		
+		console.log("IN PROFILE CONTROLLER");
+		$scope.userConfig = $scope.getUserConfig();
+		console.log($scope.userConfig);
 	});
 })();
